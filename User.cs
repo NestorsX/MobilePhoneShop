2d684031da78cfb2a7aa53271f5d6c2649ec7ab7
Кирилл Нестерович <nesterkalol@gmail.com>
2021-03-28 12:36:21 +0300

    Added class "User"

---
+++ User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhoneShop
+{
+    class User
+    {
+        private int id { get; set; }
+        private string login, password;
+        private int DataId { get; set; }
+        public User() { }
+        public User(string login, string password, int DataId)
+        {
+            this.login = login;
+            this.password = password;
+            this.DataId = DataId;
+        }
+    }
+}
