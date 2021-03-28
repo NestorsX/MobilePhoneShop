b6db5d3a45233b98874ac54c4fb26ea929563e03
Кирилл Нестерович <nesterkalol@gmail.com>
2021-03-28 13:13:39 +0300

    Changes in Registration Window

--- Register.xaml.cs
+++ Register.xaml.cs
@@ -18,16 +18,33 @@ namespace MobilePhoneShop
 {
     public partial class Register : Window
     {
-        AccessToDB acdb = new AccessToDB();
+        AppContext appContext;
+        AccessToDB acdb;
         public Register()
         {
             InitializeComponent();
             Closing += OnWindowClosing;
+            acdb = new AccessToDB();
+            appContext = new AppContext();
         }
         private void OnWindowClosing(object sender, CancelEventArgs e)
         {
             acdb.Insert($"INSERT INTO данныеПользователей (Имя, [e-mail], ДатаРегистрации) VALUES ('Вася','ksh@gmail.com', '{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.f")}')");
             Application.Current.MainWindow.Show();
+        }
+
+        private void Register_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = Login_TextBox.Text;
+            string password = Password_TextBox.Password;
+            string repeatPassword = RepeatPassword_TextBox.Password;
+            string email = Email_TextBox.Text;
+            int dataID = 0;
+
+            User user = new User(login, password, dataID);
+            
+            appContext.Users.Add(user);
+            appContext.SaveChanges();
         }
     }
 }
--- User.cs
+++ User.cs
@@ -11,6 +11,16 @@ namespace MobilePhoneShop
         private int id { get; set; }
         private string login, password;
         private int DataId { get; set; }
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
         public User() { }
         public User(string login, string password, int DataId)
         {
